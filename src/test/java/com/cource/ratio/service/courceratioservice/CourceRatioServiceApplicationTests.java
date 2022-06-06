fd8a28d6261cffdfc7e78f38c8aeb422b1490ff8
btwlav <kilusm@mail.ru>
2022-06-07 02:46:15 +0300

    creating a repository

---
+++ src/test/java/com/cource/ratio/service/courceratioservice/CourceRatioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cource.ratio.service.courceratioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourceRatioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
