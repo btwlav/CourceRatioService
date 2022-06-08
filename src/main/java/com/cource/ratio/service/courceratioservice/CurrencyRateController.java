645a7bfa92310dcfecf6abba9690276d886469b9
Студент <Student@College.edu>
2022-06-08 12:44:15 +0300

    redirecting to gif

--- src/main/java/com/cource/ratio/service/courceratioservice/CurrencyRateController.java
+++ src/main/java/com/cource/ratio/service/courceratioservice/CurrencyRateController.java
@@ -1,17 +1,26 @@
 package com.cource.ratio.service.courceratioservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.json.simple.JSONObject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 
 @RestController
 public class CurrencyRateController {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private final CurrencyRateProxy proxy;
 
     public CurrencyRateController(CurrencyRateProxy proxy) {
@@ -19,18 +28,17 @@ public class CurrencyRateController {
     }
 
     @GetMapping(value = "course-ratio/from/USD/to/{currency}")
-    public JSONObject getCourseRatio(@PathVariable("currency") String currency) throws JsonProcessingException {
+    public ModelAndView getCourseRatio(@PathVariable("currency") String currency) {
 
-        JSONObject response = proxy.getCurrencyRates(currency);
-        logger.info("{}", response);
-////        JSONArray ratesArray = (JSONArray) response.get("rates");
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode jsonNode = mapper.readTree(response.toJSONString());
-        Double rate = 62.05;//jsonNode.get("RUB").asDouble();
-        return response;
-//        return new CurrencyRate("11", "USD", rate);
-//        return new CurrencyRate(response.get("timestamp").toString(),
-//                response.get("base").toString(),
-//                rate);
+        JSONObject currentRateJSON = new JSONObject(proxy.getCurrencyRates(currency));
+        JSONObject historicalRateJSON = new JSONObject(proxy.getHistoricalRate(LocalDate.now().minusDays(2).toString(), currency));
+        Object currentValue = currentRateJSON.getJSONObject("rates").get(currency);
+        Object historicValue = historicalRateJSON.getJSONObject("rates").get(currency);
+//        System.out.println(rate);
+        if (!currentValue.equals(historicValue)) {
+            return new ModelAndView("redirect:https://api.giphy.com/v1/gifs/random?api_key=3ACtdhmayOh9xyMYDCZol32dCV1Bf4oV&tag=rich");
+        }
+
+        return new ModelAndView("redirect:https://api.giphy.com/v1/gifs/random?api_key=3ACtdhmayOh9xyMYDCZol32dCV1Bf4oV&tag=broke");
     }
 }
--- src/main/java/com/cource/ratio/service/courceratioservice/CurrencyRateProxy.java
+++ src/main/java/com/cource/ratio/service/courceratioservice/CurrencyRateProxy.java
@@ -8,5 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value="openexchangerate", url="https://openexchangerates.org")
 public interface CurrencyRateProxy {
     @GetMapping("api/latest.json?app_id=b933a73d175a4aaf954d513e39e1a4bf&symbols={currency}")
-    JSONObject getCurrencyRates(@PathVariable("currency") String currency);
+    String getCurrencyRates(@PathVariable("currency") String currency);
+
+    @GetMapping("api/historical/{date}.json?app_id=b933a73d175a4aaf954d513e39e1a4bf&symbols={currency}")
+    String getHistoricalRate(@PathVariable("date") String date, @PathVariable("currency") String currency);
 }
